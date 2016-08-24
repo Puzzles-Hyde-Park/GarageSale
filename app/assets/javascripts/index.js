e7646473565f34bb2d657c457f7d948e6266b03a
Griffin Cox <griffincox@gmail.com>
2016-08-24 18:50:47 -0500

    done styling index for now

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
     $("#login-button").click(function(){
         $(".collapse#sign-up").collapse('hide');
+        $("#sign-up-button").addClass('hidden');
     });
     $("#sign-up-button").click(function(){
         $(".collapse#login").collapse('hide');
+        $("#login-button").addClass('hidden');
+    });
+    $(".row").not(".btn").click(function(){
+        $("#sign-up-button").removeClass('hidden');
+        $("#login-button").removeClass('hidden');
     });
 });
