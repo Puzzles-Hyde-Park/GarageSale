adfa2846e293843938d60da1c750e84de2b577e2
Vladimir Surganov <avsurganov@gmail.com>
2016-08-24 13:47:02 -0500

    moved nav items, added user variable to pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_user
 
   def index
   end
@@ -14,5 +15,10 @@ class PagesController < ApplicationController
   end
 
   def privacy
+  end
+
+  private
+  def find_user
+    @user = User.find_by(session[:user_id])
   end
 end
