314f6590bc88a8ee69e2d7476712d1b345bcac92
Vladimir Surganov <avsurganov@gmail.com>
2016-08-24 16:03:40 -0500

    added styles to sign up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_user
 
   def index
   end
@@ -15,10 +14,5 @@ class PagesController < ApplicationController
   end
 
   def privacy
-  end
-
-  private
-  def find_user
-    @user = User.find_by(session[:user_id])
   end
 end
