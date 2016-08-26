39a723020eb0c51e639c0c776e12fc483f923c6a
Griffin Cox <griffincox@gmail.com>
2016-08-26 17:16:29 -0500

    got first_name, not link or photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,27 +7,8 @@ class User < ActiveRecord::Base
   # validates :username, :presence => true,
   #           :uniqueness => {:case_sensitive => false}
 
-  validate :validate_username
 
   has_many :items
-
-  def to_param
-    username
-  end
-
-  def login=(login)
-    @login = login
-  end
-
-  def login
-    @login || self.username || self.email
-  end
-
-  def validate_username
-    if User.where(email: username).exists?
-      errors.add(:username, :invalid)
-    end
-  end
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -42,19 +23,6 @@ class User < ActiveRecord::Base
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.password = Devise.friendly_token[0,20]
       user.save!
-    end
-  end
-
-  def self.find_first_by_auth_conditions(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    else
-      if conditions[:username].nil?
-        where(conditions).first
-      else
-        where(username: conditions[:username]).first
-      end
     end
   end
 end
